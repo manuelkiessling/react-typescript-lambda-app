995d7ef33c06e01c6b8c1f79349accb85cdf3546
Manuel Kießling <manuel@kiessling.net>
2021-04-26 15:49:55 +0200

    Make deploy script fail if there is an error

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+set -e
+
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 
 if [ ! -s "$HOME/.nvm/nvm.sh" ]
@@ -19,31 +21,31 @@ echo "$DEPLOYMENT_NUMBER" > "$DIR/../deployment_number"
 
 PROJECT_NAME="$(cat "$DIR/../infrastructure/variables.tf" | grep 'project_name' -A 2 | grep 'default' | cut -d '=' -f 2 | cut -d '"' -f 2)"
 
-pushd "$DIR/../frontend" || exit
+pushd "$DIR/../frontend"
   rm -rf build
   nvm install
   nvm use
   npm i --no-save
   npm run build
   aws s3 cp --recursive --acl public-read build/ "s3://$PROJECT_NAME-frontend/"
-popd || exit
+popd
 
 [ "$1" == "frontend" ] && exit 0
 
-pushd "$DIR/../backend/" || exit
+pushd "$DIR/../backend/"
   rm -rf build
   rm -rf node_modules
   nvm install
   nvm use
   npm i --target_arch=x64 --target_platform=linux --target_libc=glibc --no-save
   npm run build
-  pushd build || exit
+  pushd build
     zip -r rest_api.zip ./
     aws s3 cp ./rest_api.zip "s3://$PROJECT_NAME-backend/$DEPLOYMENT_NUMBER/"
-  popd || exit
-popd || exit
+  popd
+popd
 
-pushd "$DIR/../infrastructure" || exit
+pushd "$DIR/../infrastructure"
   terraform init
   terraform apply -auto-approve -var deployment_number="$DEPLOYMENT_NUMBER"
-popd || exit
+popd
